b934069ac9fa77f842622d5cab8bb8875f51baaf
Caleb <caleb.li.wheeler@gmail.com>
2021-08-11 21:08:15 -0400

    Built Global Descriptor Table Handler and transitioned project into protected mode

--- boot.asm
+++ boot.asm
@@ -1,11 +1,14 @@
 ; Sets the origin to 0x7c00 as this is where the BIOS loads the bootloader
-ORG 0
+ORG 0x7c00
 
 ; Specifies that this is a 16-bit architecture, meaning data comes in 16 bits
 BITS 16
 
 ; For more information on interrupts, visit http://www.ctyme.com/rbrown.htm
 ; For more information on FAT BIOS Param Block specs, visit https://wiki.osdev.org/FAT
+
+CODE_SEG equ gdt_code - gdt_start ; Will store the offset 0x8
+DATA_SEG equ gdt_data - gdt_start ; Will store the offset 0x10
 
 _start:
     jmp short start
@@ -14,77 +17,71 @@ _start:
 times 33 db 0 ; BIOS Parameter Block outside of the first 3 short jump NOP
 
 start:
-    jmp 0x7c0:step2 ; Changes the code segment to 0x7c0
+    jmp 0:step2 ; Changes the code segment to 0x7c0
 
 step2:
     cli ; Clear Interrupts
 
-    mov ax, 0x7c0 ; To modify the data segment register, ax must be set
+    mov ax, 0x00 ; To modify the data segment register, ax must be set
 
     ; Sets the data segment to the location where the BIOS loads the bootloader
     ; lodsb indexes data with the data segment and offsets with the si register
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-
     sti ; Enables Interrupts
 
-    mov ah, 2 ; Command for reading sector for CHS
-    mov al, 1 ; One sector to read
-    mov ch, 0 ; Cylinder low 8 bits
-    mov cl, 2 ; Read sector 2
-    mov dh, 0 ; Head number
-    mov bx, buffer
-    int 0x13 ; Interrupt that invokes the read command
-    jc error
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32 ; Jumps to the code segment to get the offset, then jumps to load32
 
-    mov si, buffer
-    call print
+; Global Descriptor Table
+; For more information, visit
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
+; Offset 0x8
+gdt_code:     ; Code Segment points to this
+    dw 0xffff ; Segment limit to first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte (bit mask)
+    db 1100111b ; High 4 bit flags and low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+; Offset 0x10
+gdt_data:     ; Should be pointed at by DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit to first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte (bit mask)
+    db 1100111b ; High 4 bit flags and low 4 bit flags
+    db 0      ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start ; Is the offset
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-    ; Has the bootloader jump to itself so that it doesn't execute its own
-    ; signature.
-    jmp $
-
-print:
-    ; Specifies background color and foreground
-    mov bx, 0
-
-.loop:
-    ; Loads the character that the si register points to into the al register,
-    ; and increments the si register
-    lodsb
-
-    ; If the null byte is reached, meaning si is at the end of the string, then
-    ; jump to the .done routine. Else, prints the character
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    ; Moves 0eh into the ah register, which is a part of eax.
-    ; 0eh is a command for a BIOS routine for video teletype output
-    mov ah, 0eh
-
-    ; This is an interrupt that calls the BIOS routine specified in the ah
-    ; register.
-    int 0x10
-
-    ret
-
-error_message: db 'Failed to load sector', 0
 
 ; Says that at least 510 bytes need to be used.
 ; If less than 510 bytes is filled, the rest is padded with 0's
@@ -94,5 +91,3 @@ times 510- ($ - $$) db 0
 ; Since Intel Architectures are Little Endian, the reading order
 ; is reversed. This code loads the signature into the file.
 dw 0xAA55
-
-buffer:
